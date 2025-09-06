73be30a1bac04ffb0ae246e6be7e77ec963aa847
Obiora Igwilo <obiora.igwilo@cyberspace.net.ng>
2025-09-06 15:30:50 +0100

    Stagged all

---
+++ DevConference.Shared/Models/Dtos/Conferences.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevConference.Shared.Models.Dtos
+{
+    public class Conferences
+    {
+            public string? Title { get; set; }
+            public string? Description { get; set; }
+            public DateTime StartDate { get; set; }
+            public DateTime EndDate { get; set; }
+            public string? Location { get; set; }
+            public string? ImageUrl { get; set; }
+            public string? WebsiteUrl { get; set; }
+            public bool IsActive { get; set; }
+            public int ConferenceStatus { get; set; } //1 - Upcoming, 2 - Ongoing, 3 - Completed                                                      
+        
+    }
+}
--- DevConference.Shared/Models/Entities/Conferences.cs
+++ DevConference.Shared/Models/Entities/Conferences.cs
@@ -9,7 +9,7 @@ namespace DevConference.Shared.Models.Entities
 {
     public class Conferences
     {
-        [Key]
+        
         public int Id { get; set; }
         public string? Title { get; set; }
         public string? Description { get; set; }
